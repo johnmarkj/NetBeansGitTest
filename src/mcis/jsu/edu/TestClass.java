898e01e20bc59f3d1591ccfc1eb0bf44fcbff8c5
John Jones <jjones42@stu.jsu.edu>
2018-02-20 13:25:49 -0600

    "Initial Commit"

---
+++ src/mcis/jsu/edu/TestClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mcis.jsu.edu;
+
+/**
+ *
+ * @author johnm
+ */
+public class TestClass {
+   public static String testMethod1(){
+       return "Hello, World!";
+   } 
+}
