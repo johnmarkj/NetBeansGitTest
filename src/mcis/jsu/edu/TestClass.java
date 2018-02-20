a296303a24981713e93ac0368dde3ebfe4c2d78a
John Jones <jjones42@stu.jsu.edu>
2018-02-20 13:42:16 -0600

    Added JUnitTest

--- src/mcis/jsu/edu/TestClass.java
+++ src/mcis/jsu/edu/TestClass.java
@@ -13,4 +13,7 @@ public class TestClass {
    public static String testMethod1(){
        return "Hello, World!";
    } 
+   public static String testMethod2(){
+       return "Hello there, World!";
+   }
 }
